7e846289e611686e633a2beb168f7fc123b8fa8a
fengling <398534393@qq.com>
2017-10-23 19:34:07 +0800

    create new file Emp.java

---
+++ Emp.java
@@ -0,0 +1,16 @@
+public class Emp{
+   private Integer empno;
+   private String ename;
+   public void setEmpno(Integer empno){
+     this.empno=empno;
+}
+public Integer getEmpno(){
+  return this.empno;
+}
+   public void setEname(String ename){
+     this.ename=ename;
+}
+public String getEname(){
+  return this.ename;
+}
+}
